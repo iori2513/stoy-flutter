54c5248d22697a6e7da401980c7936ecee61b18d
iori2513 <yzhongtian194@gmail.com>
2024-02-14 02:20:23 +0900

    feature: add bottomNavBarItem

--- lib/constants/app_color.dart
+++ lib/constants/app_color.dart
@@ -5,4 +5,5 @@ class AppColor {
   static const Color clearWhite = Color(0xFFFFFFFF);
   static const Color borderColor = Color(0xFFDCDDE2);
   static const Color impactBlue = Color(0xFF406DEE);
+  static const Color grey = Color(0xFF73798C);
 }
