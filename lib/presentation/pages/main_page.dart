c3657eb87765ba15a12bd022c9124c92a46d394c
iori2513 <yzhongtian194@gmail.com>
2024-02-14 02:19:23 +0900

    feature: main page

--- lib/presentation/pages/main_page.dart
+++ lib/presentation/pages/main_page.dart
@@ -1,13 +1,96 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
+import 'package:stoy/constants/app_color.dart';
+import 'package:stoy/presentation/pages/home_page.dart';
+import 'package:stoy/presentation/pages/profile_page.dart';
 
-class MainPage extends ConsumerWidget {
+class MainPage extends ConsumerStatefulWidget {
   const MainPage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold(
-      body: Text('main'),
+  ConsumerState<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends ConsumerState<MainPage> {
+  // TabItem currentTab = TabItem.home;
+
+  int selectedIndex = 0;
+
+  final _controller = PersistentTabController(initialIndex: 0);
+
+  @override
+  Widget build(BuildContext context) {
+    // final selectedTabItem = _items[selectedIndex];
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('STOY',
+            style: TextStyle(
+                color: AppColor.primaryColor,
+                fontWeight: FontWeight.bold,
+                fontSize: 20)),
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {},
+        ),
+      ),
+      body: PersistentTabView(
+        context,
+        controller: _controller,
+        screens: _buildScreens(),
+        items: _navBarsItems(),
+        confineInSafeArea: true,
+        backgroundColor: AppColor.clearWhite,
+        // Default is Colors.white.
+        handleAndroidBackButtonPress: true,
+        // Default is true.
+        resizeToAvoidBottomInset: true,
+        // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
+        stateManagement: true,
+        // Default is true.
+        hideNavigationBarWhenKeyboardShows: true,
+        // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+        decoration: NavBarDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          colorBehindNavBar: AppColor.clearWhite,
+        ),
+        popAllScreensOnTapOfSelectedTab: true,
+        popActionScreens: PopActionScreensType.all,
+        itemAnimationProperties: const ItemAnimationProperties(
+          // Navigation Bar's items animation properties.
+          duration: Duration(milliseconds: 200),
+          curve: Curves.ease,
+        ),
+        screenTransitionAnimation: const ScreenTransitionAnimation(
+          // Screen transition animation on change of selected tab.
+          animateTabTransition: true,
+          curve: Curves.ease,
+          duration: Duration(milliseconds: 200),
+        ),
+        navBarStyle:
+            NavBarStyle.style6, // Choose the nav bar style with this property.
+      ),
     );
   }
+}
+
+List<Widget> _buildScreens() {
+  return [const HomePage(), const ProfilePage()];
+}
+
+List<PersistentBottomNavBarItem> _navBarsItems() {
+  return [
+    PersistentBottomNavBarItem(
+      icon: const Icon(Icons.home),
+      title: ("Home"),
+      activeColorPrimary: AppColor.primaryColor,
+      inactiveColorPrimary: AppColor.grey,
+    ),
+    PersistentBottomNavBarItem(
+      icon: const Icon(Icons.person),
+      title: ("Profile"),
+      activeColorPrimary: AppColor.primaryColor,
+      inactiveColorPrimary: AppColor.grey,
+    ),
+  ];
 }
