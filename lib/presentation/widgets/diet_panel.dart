f5871eceae5e665c285ea6cd54c061b03b71e707
iori2513 <yzhongtian194@gmail.com>
2024-02-15 03:12:35 +0900

    feature: diet panel

---
+++ lib/presentation/widgets/diet_panel.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:stoy/constants/app_color.dart';
+
+class DietPanel extends StatelessWidget {
+  const DietPanel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 96.h,
+      width: 327.w,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15.h),
+        color: AppColor.borderColor,
+      ),
+      child: Row(
+        children: [
+          SizedBox(
+            width: 10.w,
+          ),
+          Container(
+            height: 61.h,
+            width: 82.w,
+            decoration:
+                BoxDecoration(borderRadius: BorderRadius.circular(15.h)),
+            child: Image.network('https://picsum.photos/250?image=9'),
+          ),
+          SizedBox(
+            width: 10.w,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 10.h,
+              ),
+              const Text(
+                '1食目',
+                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 12),
+              ),
+              SizedBox(
+                height: 5.h,
+              ),
+              const Text(
+                'タンパク質: 500g',
+                style: TextStyle(fontSize: 10, color: AppColor.textGrey),
+              ),
+              const Text(
+                '脂質: 400g',
+                style: TextStyle(fontSize: 10, color: AppColor.textGrey),
+              ),
+              const Text(
+                '炭水化物: 500g',
+                style: TextStyle(fontSize: 10, color: AppColor.textGrey),
+              ),
+              SizedBox(
+                height: 10.h,
+              ),
+            ],
+          ),
+          const Spacer(),
+          const Icon(
+            Icons.arrow_forward_ios,
+            size: 20,
+          ),
+          SizedBox(
+            width: 20.w,
+          )
+        ],
+      ),
+    );
+  }
+}
