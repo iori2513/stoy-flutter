6d27a8fefea7b89d55a44eecf30c23139073296b
iori2513 <yzhongtian194@gmail.com>
2024-02-15 03:12:47 +0900

    feature: add style

--- lib/constants/app_color.dart
+++ lib/constants/app_color.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 
 class AppColor {
   static const Color primaryColor = Color(0xFF406DEE);
+  static const Color secondaryColor = Color(0xFF324A59);
   static const Color clearWhite = Color(0xFFFFFFFF);
   static const Color borderColor = Color(0xFFDCDDE2);
   static const Color impactBlue = Color(0xFF406DEE);
   static const Color grey = Color(0xFF73798C);
+  static const Color backGroundGrey = Color(0xFFF7F8FB);
+  static const Color textGrey = Color(0xFF757575);
 }
