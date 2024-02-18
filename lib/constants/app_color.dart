028e1628a770c78e9153e2641c1a2581cd0e949e
iori2513 <yzhongtian194@gmail.com>
2024-02-18 20:56:40 +0900

    feature: add color

--- lib/constants/app_color.dart
+++ lib/constants/app_color.dart
@@ -7,6 +7,8 @@ class AppColor {
   static const Color borderColor = Color(0xFFDCDDE2);
   static const Color impactBlue = Color(0xFF406DEE);
   static const Color grey = Color(0xFF73798C);
-  static const Color backGroundGrey = Color(0xFFF7F8FB);
+
+  // static const Color backGroundGrey = Color(0xFFF7F8FB);
+  static const Color backGroundGrey = Color(0xFFF5F6FA);
   static const Color textGrey = Color(0xFF757575);
 }
