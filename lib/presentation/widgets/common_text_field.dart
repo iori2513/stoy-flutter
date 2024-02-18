21309649f60eef58453f07bfadff785d540f7dd9
iori2513 <yzhongtian194@gmail.com>
2024-02-18 20:49:36 +0900

    feature: add widgets

---
+++ lib/presentation/widgets/common_text_field.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:stoy/constants/app_color.dart';
+
+class CommonTextField extends StatefulWidget {
+  final void Function(String value) onChangeText;
+  final String placeholder;
+  final double height;
+  final double width;
+  final int? minLines;
+  final int? maxLines;
+
+  const CommonTextField(
+      {super.key,
+      required this.onChangeText,
+      this.placeholder = '',
+      required this.height,
+      required this.width,
+      this.minLines = 1,
+      this.maxLines});
+
+  @override
+  State<CommonTextField> createState() => _CommonTextField();
+}
+
+class _CommonTextField extends State<CommonTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: widget.width,
+      height: widget.height,
+      child: TextFormField(
+        textAlignVertical: TextAlignVertical.center,
+        onChanged: widget.onChangeText,
+        cursorColor: AppColor.primaryColor,
+        decoration: InputDecoration(
+          fillColor: AppColor.backGroundGrey,
+          filled: true,
+          contentPadding: EdgeInsets.symmetric(vertical: 4.h, horizontal: 8.w),
+          enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: AppColor.clearWhite, width: 1),
+              borderRadius: BorderRadius.all(Radius.circular(10.0))),
+          focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: AppColor.primaryColor),
+              borderRadius: BorderRadius.all(Radius.circular(10.0))),
+          hintText: widget.placeholder,
+          hintStyle: const TextStyle(color: AppColor.textGrey, fontSize: 13),
+        ),
+        keyboardType: TextInputType.multiline,
+        maxLines: widget.minLines,
+        minLines: widget.minLines,
+      ),
+    );
+  }
+}
---
+++ lib/presentation/widgets/input_num_field.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:stoy/constants/app_color.dart';
+
+class InputNumField extends StatefulWidget {
+  final void Function(double value) onChangeNum;
+  final String placeholder;
+  final double height;
+  final double width;
+
+  const InputNumField({
+    super.key,
+    required this.onChangeNum,
+    this.placeholder = '',
+    required this.height,
+    required this.width,
+  });
+
+  @override
+  State<InputNumField> createState() => _InputNumFieldState();
+}
+
+class _InputNumFieldState extends State<InputNumField> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: widget.width,
+      height: widget.height,
+      child: TextFormField(
+        textAlignVertical: TextAlignVertical.center,
+        onChanged: (String val) {
+          widget.onChangeNum(double.parse(val));
+        },
+        cursorColor: AppColor.primaryColor,
+        decoration: InputDecoration(
+          fillColor: AppColor.backGroundGrey,
+          filled: true,
+          contentPadding: EdgeInsets.symmetric(vertical: 4.h, horizontal: 8.w),
+          enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: AppColor.clearWhite, width: 1),
+              borderRadius: BorderRadius.all(Radius.circular(10.0))),
+          focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: AppColor.primaryColor),
+              borderRadius: BorderRadius.all(Radius.circular(10.0))),
+          hintText: widget.placeholder,
+          hintStyle: const TextStyle(color: AppColor.textGrey, fontSize: 13),
+        ),
+        keyboardType: TextInputType.number,
+      ),
+    );
+  }
+}
