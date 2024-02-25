0c6a7533d4d1b4a5228d1e8bbd12fe53fe7c1782
iori2513 <yzhongtian194@gmail.com>
2024-02-25 21:41:17 +0900

    feature: auth check

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:stoy/presentation/pages/auth/sign_in_page.dart';
 import 'package:stoy/presentation/pages/auth/sign_up_page.dart';
-import 'package:stoy/presentation/pages/core/default_page.dart';
+import 'package:stoy/presentation/pages/core/auth_check.dart';
 import 'package:stoy/presentation/pages/core/home_page.dart';
 import 'package:stoy/presentation/pages/core/main_page.dart';
 import 'package:stoy/presentation/pages/core/profile_page.dart';
--- lib/presentation/pages/core/auth_check.dart
+++ lib/presentation/pages/core/auth_check.dart
@@ -1,40 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:stoy/app_router.dart';
 import 'package:stoy/domain/entities/auth/auth_user.dart';
 import 'package:stoy/presentation/pages/core/main_page.dart';
 import 'package:stoy/presentation/providers/shared/auth/auth_provider.dart';
-
-class DefaultPage extends ConsumerWidget {
-  const DefaultPage({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final authUser = ref.watch(authUserProvider);
-    final authNotifier = ref.watch(authNotifierProvider.notifier);
-    return authUser.when(
-        data: (AuthUser? user) {
-          if (user != null) {
-            print(12334);
-            Future.microtask(() => authNotifier.setUser(user));
-            return const MainPage();
-          } else {
-            return const Navigator(
-              initialRoute: 'singIn',
-              onGenerateRoute: AppRouter.generateRoute,
-            );
-          }
-        },
-        error: (error, _) {
-          print(error);
-          return const Navigator(
-            initialRoute: 'singIn',
-            onGenerateRoute: AppRouter.generateRoute,
-          );
-        },
-        loading: () => const CircularProgressIndicator());
-  }
-}
 
 class AuthCheck extends ConsumerWidget {
   final Widget loggedInScreen;
