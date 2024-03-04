060418246992682064a0762dedc8bec98c18d90f
iori2513 <yzhongtian194@gmail.com>
2024-03-05 01:10:47 +0900

    feature: body repository impl

---
+++ lib/data/repositories/body/body_repository_impl.dart
@@ -0,0 +1,25 @@
+import 'package:stoy/data/data_sources/body/body_remote_data_source.dart';
+import 'package:stoy/domain/entities/body/body.dart';
+import 'package:stoy/domain/repositories/body/body_repository.dart';
+
+class BodyRepositoryImpl implements BodyRepository {
+  final BodyRemoteDataSource remoteDataSource:
+
+  const BodyRepositoryImpl(this.remoteDataSource);
+
+  @override
+  Future<void> addBodyData(Body body) {
+    return remoteDataSource.addBodyData(body);
+  }
+
+  @override
+  Stream<List<Body>> bodyDataList(String userId) {
+    return remoteDataSource.fetchBodyDataList(userId);
+  }
+
+  @override
+  Future<void> updateBodyData(Body body) {
+    return remoteDataSource.updateDiet(body: body);
+  }
+
+}
